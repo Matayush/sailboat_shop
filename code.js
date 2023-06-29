d27b0696e9e989a8f5a806ce42e5a4aa71939133
Mateusz Ceniuk <ceniuk.mateusz@gmail.com>
2023-06-29 12:12:10 +0200

    code.js changed so the server displayse a picture of a sailboat

--- code.js
+++ code.js
@@ -1,10 +1,9 @@
 const http = require('http');
 const port = process.env.PORT || 8080;
 
-
 const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('sailboat_shop!');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<html><body><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVAo-xNljd7FbADI8Y8T0WNh9qzouIGQ9XIg&usqp=CAU" alt="sailboat"></body></html>');
 }
 
 const server = http.createServer(requestListener);
